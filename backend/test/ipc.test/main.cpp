a9dd9e0d114de016114d8273082afde73226b19d
JiangDunchun <jiangdunchun@outlook.com>
2026-02-06 16:32:11 +0800

    use ecal

--- backend/test/ipc.test/main.cpp
+++ backend/test/ipc.test/main.cpp
@@ -1,3 +1,21 @@
-int main() {
+#include <iostream>
+#include <string>
+#include <thread>
+#include <ecal/ecal.h>
+#include <ecal/msg/string/publisher.h>
+
+int main(int argc, char** argv) {
+    std::cout << "===start service in /ipc.test===" << std::endl;
+
+    eCAL::Initialize(0, nullptr, "simple_publisher");
+
+    eCAL::string::CPublisher<std::string> pub("demo_topic");
+
+    while (eCAL::Ok()) {
+        pub.Send("Hello from eCAL!");
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    eCAL::Finalize();
     return 0;
 }
